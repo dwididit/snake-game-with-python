f2e77b47eab753b68b9c430745acb9c1b4edd604
Dwi Didit Prasetiyo <didit@dwidi.com>
2023-12-05 19:51:25 +0700

    Adding file to GitHub

---
+++ main.py
@@ -0,0 +1,82 @@
+#import turtle
+import turtle
+from turtle import Screen
+import time
+#import snake class
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+#create a screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+#create snake class
+snake = Snake()
+
+# control the snake
+screen.listen()
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+
+#create a food
+food = Food()
+
+#create a scoreboard
+scoreboard = Scoreboard()
+
+
+#move the snake
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+        if scoreboard.score == 30:
+            game_is_on = False
+            scoreboard.win()
+
+
+    #detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor()< -280:
+        scoreboard.game_over()
+
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+
+
+
+
+
+
+
+# exit on space bar
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scoreboard.py
@@ -0,0 +1,28 @@
+#create scoreboard class
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.goto(0, 270)
+        self.color("white")
+        self.hideturtle()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}, High Score: {self.high_score}", align="center", font=("Arial", 24, "normal"))
+
+
+    def game_over(self):
+        self.penup()
+        self.goto(0,0)
+        self.write("YOU LOSE!", align="center", font=("Arial", 24, "normal"))
+
+
+    def win(self):
+        self.penup()
+        self.goto(0,0)
+        self.write("YOU WIN", align="center", font=("Arial", 24, "normal"))
